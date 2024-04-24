c0001db6fa6a4bae9c93dd193f5fb4973dd71abf
Kinghope007 <emmanuelhope39@gmail.com>
2024-04-24 20:34:57 +0000

    correction

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,6 @@
-const handleResponseFromAPI = (promise) => {
+export default function handleResponseFromAPI(promise) {
   return promise
     .then(() => ({ status: 200, body: 'success' }))
     .catch(() => Error())
     .finally(() => console.log('Got a response from the API'));
 }
-
-export default handleResponseFromAPI;
