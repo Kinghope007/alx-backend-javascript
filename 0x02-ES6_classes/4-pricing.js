d64c167871b15cbfabd1483f614a16f194bde8a4
Emmanuel Hope Joel <emmanuelhope39@gmail.com>
2024-04-25 23:36:09 +0000

    importing and using another class

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,1 +1,44 @@
+import Currency from './3-currency.js';
 
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (typeof value !== 'number') {
+        throw new TypeError('Amount must be a number')
+    }
+    this._amount = value;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+        throw new TypeError('currency must be Currency');
+    }
+    this._currency = value
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency._name} (${this.currency._code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    if (typeof conversionRate !== 'number') {
+        throw new TypeError('conversionRate must be a number');
+    }
+    return amount * conversionRate;
+  }
+}
