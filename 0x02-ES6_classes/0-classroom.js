d21d5f9251e2c5b4c51dbb97b29bb6df6b0e0ea6
Kinghope007 <emmanuelhope39@gmail.com>
2024-04-25 21:48:47 +0000

    ES6 class with a constructor and using this.

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,13 @@
+/**
+ * REspresnt a class
+ */
+export default class ClassRoom {
+  /**
+   * Create a new class
+   * @params {number maxStudentSize - ma no
+   * student in class.
+   */
+  constructor(maxStudentsSize) {
+     this._maxStudentsSize = maxStudentsSize;
+   }
+}
